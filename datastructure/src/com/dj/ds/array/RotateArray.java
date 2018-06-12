1efaddbb1e8ee87675a51589541c7de4f1d82994
djshrshiv <dijadhav@dijadhav-ThinkPad-T440>
2018-06-12 20:29:43 +0530

    Created the project
    created the project

---
+++ datastructure/src/com/dj/ds/array/RotateArray.java
@@ -0,0 +1,14 @@
+package com.dj.ds.array;
+
+public class RotateArray {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		rotate1(arr, arr.length, 2);
+	}
+
+	private static void rotate1(int[] arr, int length, int i) {
+
+	}
+
+}
