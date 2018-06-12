35d27951d76b4cc789a13396c82f01815ed58b67
djshrshiv <dijadhav@dijadhav-ThinkPad-T440>
2018-06-12 20:52:45 +0530

    added 1st implementation to rotate the array

--- datastructure/src/com/dj/ds/array/RotateArray.java
+++ datastructure/src/com/dj/ds/array/RotateArray.java
@@ -1,14 +1,72 @@
 package com.dj.ds.array;
 
+/**
+ * 
+ * This class implements the array rotation functionality
+ * 
+ * @author Dipak Jadhav
+ *
+ */
 public class RotateArray {
-
+	/**
+	 * Main method of the application
+	 * 
+	 * @param args
+	 *            - Array of command line arguments
+	 */
 	public static void main(String[] args) {
 		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		display(arr);
+		// Rotate the array by two numbers
 		rotate1(arr, arr.length, 2);
+		display(arr);
 	}
 
-	private static void rotate1(int[] arr, int length, int i) {
+	/**
+	 * Display array elements
+	 * 
+	 * @param arr
+	 *            - Array of integers
+	 */
+	private static void display(int[] arr) {
+		if (null != arr && arr.length > 0) {
+			for (int n : arr) {
+				System.out.print(n + " ");
+			}
+		}
+		System.out.println();
+	}
 
+	/**
+	 * Rotate the array
+	 * 
+	 * @param arr
+	 *            - Array to be rotated
+	 * @param n
+	 *            - Length of array
+	 * @param d
+	 *            - Number by which array is rotated
+	 */
+	private static void rotate1(int[] arr, int n, int d) {
+		if (null == arr || n <= 0 || d <= 0) {
+			System.out.println("Invalid input");
+			return;
+		}
+		int k = 0;
+		// Create temporary array
+		int[] temp = new int[d];
+		for (; k < temp.length; k++) {
+			temp[k] = arr[k];
+		}
+		int j = 0;
+		for (; j < n - d; j++) {
+			arr[j] = arr[k];
+			k++;
+		}
+		for (int i = 0; i < temp.length; i++) {
+			arr[j] = temp[i];
+			j++;
+		}
 	}
 
 }
